fbb4ea390b3db9c5671760f39dcc426169f075f6
Arnauld Van Muysewinkel <arnauldvm@gmail.com>
2018-07-06 14:41:20 +0200

    add(data): add contracts table

--- src/main/py/load-data.py
+++ src/main/py/load-data.py
@@ -13,4 +13,12 @@ resources = pd.read_csv(path.join(datadir, 'resources.csv'), delim_whitespace=Tr
 resources.set_index('color_en', inplace=True, drop=False)
 print resources
 colors = resources.color_en
-print " ".join(colors)
+
+contracts = pd.read_csv(path.join(datadir, 'contracts.csv'), delim_whitespace=True, skiprows=[1])
+contracts['shipping'] = [
+    { color: row[color] for color in colors if row[color]!=0 }
+    for index, row in contracts.iterrows()
+]
+contracts.drop(columns=colors, inplace=True)
+contracts.set_index('kind', inplace=True)
+print contracts
